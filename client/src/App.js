5807eca6805239c96a6007e9205f66ff2fc9de9e
Jeevan Kumar <sjeevankumarjk@gmail.com>
2023-04-03 10:25:36 +0530

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Homepage";
+import Contact from "./components/Contact";
+import Adminpage from "./components/Adminpage";
+import AdminQueryview from "./components/AdminQueryview";
+// import Userpage from "./components/StudentComponents/UserProfile";
+// import Complaints from "./components/StudentComponents/Complaints";
+// import Fine from "./components/StudentComponents/Fine";
 
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AdminFineView from "./components/AdminFineView";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Home />
+      </div>
+    ),
+  },
+  {
+    path: "/admin/student-details",
+    element: <Adminpage />,
+  },
+  {
+    path: "/admin/compliants",
+    element: <AdminQueryview />,
+  },
+  {
+    path: "/admin/fine",
+    element: <AdminFineView />,
+  },
+
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  // {
+  //   path: "/user",
+  //   element: <Userpage />,
+  // },
+  // {
+  //   path: "/Complaints",
+  //   element: <Complaints />,
+  // },
+  // {
+  //   path: "/Fine",
+  //   element: <Fine />,
+  // },
+]);
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
