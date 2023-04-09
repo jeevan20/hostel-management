d82b9ac61a6189e0cbbe66eed6051d9c02470c7e
Jeevan Kumar <sjeevankumarjk@gmail.com>
2023-04-09 13:38:26 +0530

    Update mail.js

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const router = express.Router();
 const isAuth = require("../middleware/isAuth");
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   port: 465, // true for 465, false for other ports
